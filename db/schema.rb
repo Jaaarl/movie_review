7a2f7f6a66007cdf21711603611ddc99b96be195
Jaaarl <jarl13.work@gmail.com>
2024-10-12 16:54:12 +0800

    add: movie schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_12_031535) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_12_083439) do
+  create_table "movies", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "blurb"
+    t.date "released"
+    t.string "country_of_origin"
+    t.datetime "showing_start"
+    t.datetime "showing_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_12_031535) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "movies", "users"
 end
