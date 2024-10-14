ee7ea0c22af061b8e3b87d583e6d215987703a92
Jaaarl <jarl13.work@gmail.com>
2024-10-14 11:18:47 +0800

    modify: fixed model bug for movie & category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   has_many :movie_category_ships
-  has_many :movie, through: :movie_category_ships
+  has_many :movies, through: :movie_category_ships
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ApplicationRecord
   belongs_to :user
 
   has_many :movie_category_ships
-  has_many :category, through: :movie_category_Ships
+  has_many :categories, through: :movie_category_ships
 end
