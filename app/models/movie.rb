c5757bf49d4120628d1fb69976816edc51fe138b
jaaarl <jarl13.work@gmail.com>
2024-10-16 04:30:47 +0800

    add: soft delete for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,12 @@
 class Movie < ApplicationRecord
+  default_scope { where(deleted_at: nil) }
   belongs_to :user
 
   has_many :movie_category_ships
   has_many :categories, through: :movie_category_ships
   has_many :reviews
+
+ def destroy
+    update(deleted_at: Time.now)
+  end
 end
