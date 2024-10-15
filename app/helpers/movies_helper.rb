8c0d5e1bae3d7b88b7e5fd59ec2031f4dff3d998
Jaaarl <jarl13.work@gmail.com>
2024-10-15 15:17:54 +0800

    add: helper for average & user ratings

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,23 @@
 module MoviesHelper
+  def your_rating(movie)
+    unless movie.reviews.where(user_id: current_user.id).empty?
+      movie.reviews.where(user_id: current_user.id)[0]&.rating
+    else
+      0.0
+    end
+  end
+
+  def average_rating(movie)
+    arr = movie.reviews.map(&:rating)
+    ave = 0.0
+    if arr == []
+      0.00
+    else
+      arr.each do |num|
+        ave = ave + num
+      end
+      ave = ave / arr.size
+    end
+    ave
+  end
 end
