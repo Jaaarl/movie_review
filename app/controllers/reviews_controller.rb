f79aefe5ddd07ccf8565af0b884d8fea18200329
jaaarl <jarl13.work@gmail.com>
2024-10-21 02:24:31 +0800

    modify: update routes to accept alphanumeric params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,41 +1,41 @@
 class ReviewsController < ApplicationController
   def new
-    @movie = Movie.includes(reviews: :user).find(params[:movie_id])
+    @movie = Movie.includes(reviews: :user).find_by(permalink: params[:movie_permalink])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.includes(reviews: :user).find(params[:movie_id])
+    @movie = Movie.includes(reviews: :user).find_by(permalink: params[:movie_permalink])
     @review = @movie.reviews.new(review_params)
     @review.user = current_user
 
     if @review.save
-      redirect_to @movie, notice: 'Review was successfully created.'
+      redirect_to movie_path(@movie.permalink), notice: 'Review was successfully created.'
     else
       render :new
     end
   end
 
   def edit
-    @movie = Movie.includes(reviews: :user).find(params[:movie_id])
+    @movie = Movie.includes(reviews: :user).find_by(permalink: params[:movie_permalink])
     @review = @movie.reviews.find(params[:id])
   end
 
   def update
-    @movie = Movie.includes(reviews: :user).find(params[:movie_id])
+    @movie = Movie.includes(reviews: :user).find_by(permalink: params[:movie_permalink])
     @review = @movie.reviews.find(params[:id])
     if @review.update(review_params)
-      redirect_to @movie, notice: 'Review was successfully updated.'
+      redirect_to movie_path(@movie.permalink), notice: 'Review was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
-    @movie = Movie.includes(reviews: :user).find(params[:movie_id])
+    @movie = Movie.includes(reviews: :user).find_by(permalink: params[:movie_permalink])
     @review = @movie.reviews.find(params[:id])
     @review.destroy
-    redirect_to @movie, notice: 'Review was successfully deleted.'
+    redirect_to movie_path(@movie.permalink), notice: 'Review was successfully deleted.'
   end
 
   private
