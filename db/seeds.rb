67d0905f3b241601ed5c93c7250a31ec4419196b
Jaaarl <jarl13.work@gmail.com>
2024-10-21 18:33:29 +0800

    add: sample data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Category.destroy_all
+Movie.destroy_all
 genres = [
   "Action",
   "Adventure",
@@ -24,3 +26,31 @@ genres = [
 genres.each do |genre|
   Category.create(name: genre)
 end
+def random_permalink(length = 7)
+  (0...length).map { (65 + rand(26)).chr }.join + rand(1000..9999).to_s # Random letters and numbers
+end
+10.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    blurb: Faker::Movie.quote,
+    released: Faker::Date.between(from: '1900-01-01', to: '2023-01-01'),
+    country_of_origin: Faker::Address.country,
+    showing_start: Faker::Time.between(from: DateTime.now, to: DateTime.now + 1.year),
+    showing_end: Faker::Time.between(from: DateTime.now + 1.year, to: DateTime.now + 2.years),
+    user_id: User.pluck(:id).sample,
+    average_rating: Faker::Number.between(from: 1.0, to: 5.0).round(1),
+    permalink: random_permalink
+  )
+end
+
+Movie.all.each do |movie|
+  rand(1..5).times do
+    Review.create!(
+      content: Faker::Lorem.paragraph(sentence_count: rand(1..5)),
+      rating: rand(1..5),
+      user_id: 1,
+      movie_id: movie.id
+    )
+  end
+end
+
